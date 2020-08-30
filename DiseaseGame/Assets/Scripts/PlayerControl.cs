12f50731ca01b97ab362eb213aaa660c8ce516da
cjw20 <cjw20@iu.edu>
2020-08-30 14:17:01 -0400

    Added a simple dash ability

--- DiseaseGame/Assets/Scripts/PlayerControl.cs
+++ DiseaseGame/Assets/Scripts/PlayerControl.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,7 +9,10 @@ public class PlayerControl : MonoBehaviour
     public Rigidbody2D rb; //the rigid body connected to the white blood cell
     Vector2 movement; //the direction the player is inputting for movement
     public float moveSpeed; //the speed of the white blood cell's movement
-
+    bool isDashing = false;
+    public float dashModifier;  //the amount dashing speeds up movement
+    public float dashDuration; //how long the speed boost lasts
+    public float dashCooldown; //how long player has to wait before dashing again
 
     // Start is called before the first frame update
     void Start()
@@ -21,11 +25,33 @@ public class PlayerControl : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(!isDashing)  //keeps player from activating a dash while already dashing
+                StartCoroutine(DashForward());
+        }
+    }
+
+    IEnumerator DashForward()
+    {
+        isDashing = true;
+        float oldSpeed = moveSpeed; //stores old speed to return to after dash
+        moveSpeed = moveSpeed * dashModifier;
+
+        yield return new WaitForSeconds(dashDuration);
+
+        moveSpeed = oldSpeed;
+
+        yield return new WaitForSeconds(dashCooldown);
+        isDashing = false;
+
+        yield break;
     }
 
     void FixedUpdate()
     {
-        //rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        
 
         rb.velocity = movement * moveSpeed;
     }
