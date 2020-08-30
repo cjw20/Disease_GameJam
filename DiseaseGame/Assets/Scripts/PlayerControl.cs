4ec12b8974281a86b0520e0dc9e1b39e5ec17e6b
cjw20 <cjw20@iu.edu>
2020-08-30 13:44:18 -0400

    Created basic movement for white cell

---
+++ DiseaseGame/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public Rigidbody2D rb; //the rigid body connected to the white blood cell
+    Vector2 movement; //the direction the player is inputting for movement
+    public float moveSpeed; //the speed of the white blood cell's movement
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
