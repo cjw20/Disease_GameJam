7935ea05a37146510e48eb71adabd728e321ebff
cjw20 <cjw20@iu.edu>
2020-08-30 15:21:02 -0400

    added enemy with navigation ai that targets and moves towards red blood cells

---
+++ DiseaseGame/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public Rigidbody2D enemyBody;
+    Vector3 currentPosition;
+    List<GameObject> redCells = new List<GameObject>();
+    //GameObject[] redCells; //cells that bacteria are targeting
+    GameObject currentTarget;
+
+    NavMeshAgent2D agent;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent2D>();
+
+        currentPosition = transform.position;
+        redCells = GameObject.FindGameObjectsWithTag("Target").ToList(); ; //gets all remaining red cells when spawned
+        FindTarget();
+
+    }
+
+    void FindTarget()
+    {
+        //add check if list empty here!!!!
+
+        float closestDistance = Mathf.Infinity;  //sets closest distance to infinity so any other value will override it
+
+        foreach(GameObject redCell in redCells)  //goes through and finds closest red cell
+        {
+            float distance = Vector3.Distance(redCell.transform.position, transform.position);
+
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                currentTarget = redCell;
+            }
+        }
+
+        agent.destination = currentTarget.transform.position;
+    }
+
+    void Update()
+    {
+        
+    }
+}
