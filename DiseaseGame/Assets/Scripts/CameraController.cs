8efe57c72f80fe85ea882916dd8c3f77ee5e4d75
cjw20 <cjw20@iu.edu>
2020-09-03 12:25:53 -0400

    Made camera follow player

---
+++ DiseaseGame/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform playerLoc;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.position = new Vector3(playerLoc.position.x, playerLoc.position.y, playerLoc.position.z - 10);
+    }
+}
